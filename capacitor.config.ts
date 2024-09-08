134f0fab079a3d4e87742a346061514b76a88841
barend.vanrooyen <barend.vanrooyen@redstor.com>
2024-09-08 16:54:07 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'solve-qr',
+  webDir: 'dist'
+};
+
+export default config;
