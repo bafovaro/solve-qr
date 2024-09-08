af71071524001b7188a99abd3be37000b6b16514
barend.vanrooyen <barend.vanrooyen@redstor.com>
2024-09-08 18:59:02 +0200

    Add firebase hosting and github actions

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,13 +11,39 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <video ref="scanner"></video>
+      <canvas ref="qrcanvas"></canvas>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import QrScanner from "qr-scanner";
+import {onMounted, ref} from "vue";
+import QRCode from "qrcode";
+
+const scanner = ref(null);
+const qrcanvas = ref(null);
+
+onMounted(() => {
+  
+  console.log('mounted');
+  
+  const qrScanner = new QrScanner(
+      scanner.value, (result) => {
+        console.log(result);
+        qrScanner.stop();
+
+        QRCode.toCanvas(qrcanvas.value, result.data, function (error) {
+          if (error) console.error(error)
+          console.log('success!');
+        })
+      },
+      { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
+  );
+  
+  qrScanner.start();
+});
+
 </script>
